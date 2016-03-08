a56a565a5eed59e85ca6ca5a9a059fb8b08097be
Anders Tornøe <ato@kvuc.dk>
2016-03-08 12:16:47 +0100

    init commit - mockup

---
+++ taksonomi_udsagn.js
@@ -0,0 +1,52 @@
+var JsonObj;
+
+function loadData(url) {
+    $.ajax({
+        url: url,
+        // contentType: "application/json; charset=utf-8",  // Blot en test af tegnsaettet....
+        //dataType: 'json', // <------ VIGTIGT: Saadan boer en angivelse til en JSON-fil vaere! 
+        dataType: 'text', // <------ VIGTIGT: Pga. ???, saa bliver vi noedt til at angive JSON som text. 
+        async: false, // <------ VIGTIGT: Sikring af at JSON hentes i den rigtige raekkefoelge (ikke asynkront). 
+        success: function(data, textStatus, jqXHR) {
+            JsonObj = jQuery.parseJSON(data);
+            // Alt data JsonObj foeres over i arrays:
+
+
+            //$(".correct").html("Correct answers: <b>" + score + " / " + antal_korrekte + " </b> Attempts: <b>" + attempts + "</b>");
+            //next_round();
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            alert("Error!!!\njqXHR:" + jqXHR + "\ntextStatus: " + textStatus + "\nerrorThrown: " + errorThrown);
+        }
+    });
+
+    console.log("jSON loaded");
+    init();
+}
+
+function init() {
+    // GENERER DROPPABBLE OG DRAGGABLE //
+    for (var i = 0; i < JsonObj.udsagn.length; i++) {
+        $(".droppable_container").append("<div class='droppable' id=" + i + ">" + JsonObj.udsagn[i].Udsagn_sentence + "</div>");
+    }
+
+    for (var i = 0; i < JsonObj.niveauer.length; i++) {
+        $(".draggable_container").append("<h2><div class='draggable label label-default' id=" + i + ">" + JsonObj.niveauer[i] + "</div></h2>");
+    }
+    // 
+    $(".droppable").droppable({
+        drop: function(event, ui) {}
+    });
+    $(".draggable").draggable({
+        //revert: true,
+        drag: function(event, ui) {
+
+            $(this).css("opacity", 0.8).addClass("draggable-active");
+        },
+        stop: function(event, ui) {
+            $(this).css("opacity", 1).removeClass("draggable-active");
+        }
+    });
+
+
+}
