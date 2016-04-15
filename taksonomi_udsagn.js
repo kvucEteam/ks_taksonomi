c318ff01a0baab4ac9acef74198dca66e937a0a1
Anders Tornøe <ato@kvuc.dk>
2016-04-15 14:36:46 +0200

    auto vomit

--- taksonomi_udsagn.js
+++ taksonomi_udsagn.js
@@ -1,37 +1,18 @@
-var JsonObj;
-
-function loadData(url) {
-    $.ajax({
-        url: url,
-        // contentType: "application/json; charset=utf-8",  // Blot en test af tegnsaettet....
-        //dataType: 'json', // <------ VIGTIGT: Saadan boer en angivelse til en JSON-fil vaere! 
-        dataType: 'text', // <------ VIGTIGT: Pga. ???, saa bliver vi noedt til at angive JSON som text. 
-        async: false, // <------ VIGTIGT: Sikring af at JSON hentes i den rigtige raekkefoelge (ikke asynkront). 
-        success: function(data, textStatus, jqXHR) {
-            JsonObj = jQuery.parseJSON(data);
-            // Alt data JsonObj foeres over i arrays:
-
-
-            //$(".correct").html("Correct answers: <b>" + score + " / " + antal_korrekte + " </b> Attempts: <b>" + attempts + "</b>");
-            //next_round();
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            alert("Error!!!\njqXHR:" + jqXHR + "\ntextStatus: " + textStatus + "\nerrorThrown: " + errorThrown);
-        }
-    });
-
-    console.log("jSON loaded");
+$(document).ready(function(){
     init();
-}
+});
+
+
 
 function init() {
     // GENERER DROPPABBLE OG DRAGGABLE //
-    for (var i = 0; i < JsonObj.udsagn.length; i++) {
-        $(".droppable_container").append("<div class='droppable' id=" + i + ">" + JsonObj.udsagn[i].Udsagn_sentence + "</div>");
+
+    for (var i = 0; i < jsonData.udsagn.length; i++) {
+        $(".droppable_container").append("<div class='droppable' id=" + i + ">" + jsonData.udsagn[i].Udsagn_sentence + "</div>");
     }
 
-    for (var i = 0; i < JsonObj.niveauer.length; i++) {
-        $(".draggable_container").append("<h3><div class='draggable label label-default' class=label_" + i + ">" + JsonObj.niveauer[i] + "</div></h3>");
+    for (var i = 0; i < jsonData.niveauer.length; i++) {
+        $(".draggable_container").append("<h3><div class='draggable label label-default label_" + i + "'>" + jsonData.niveauer[i] + "</div></h3>");
     }
     // 
     $(".droppable, .draggable_container").droppable({
@@ -42,6 +23,9 @@ function init() {
     });
     $(".draggable").draggable({
         revert: "invalid",
+        start: function(event, ui) {
+            create_new_tag($(this));
+        },
         drag: function(event, ui) {
 
             $(this).css("opacity", 0.8).addClass("draggable-active");
@@ -52,4 +36,11 @@ function init() {
     });
 
 
+}
+
+
+function create_new_tag(obj){
+    var indeks = obj.attr("class").split(" ")[3];
+    indeks = indeks[6]
+    console.log(indeks);
 }
